86c63115cad2317c016b4d6de43eacd8993d29a8
MateusMarques28 <92175534+MateusMarques28@users.noreply.github.com>
2021-10-28 16:47:09 -0300

    Delete Conec.php

--- Conec.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try{
-    $conec = new PDO("mysql:host=localhost; dbname=crud2", "root");
-    $conec-> exec("set names utf8");
-}
-catch(PDOExcption $erro){
-    echo "Erro de execução" .$erro->getMessage();
-}
